bf9f6fd6b12dbdfa2a92ef119735de182670a04f
AbdulhameedMakhdum <darkwarrior739186@gmail.com>
2025-12-12 01:08:34 +0300

    Delete db.php

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dsn = 'sqlite:sample_products.db';
-try {
-    $pdo = new PDO($dsn);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
